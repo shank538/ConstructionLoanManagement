6fd18348637e2d306159f149986143e998cbe369
SHANK-PC\Shashank <shank538@gmail.com>
2017-09-26 23:29:34 +0800

    col name change

---
+++ dbscripts/ConstructionLoan.sql
@@ -0,0 +1,16 @@
+
+
+select * from constructionloanmanagement.address;
+
+insert into constructionloanmanagement.address
+ values(5,'street2','211004','Kanpur','IN','45.3','76.1');
+ 
+insert into constructionloanmanagement.address values('streer2','211003','alld','IN','56.9','58.1');
+
+drop table constructionloanmanagement.loan;
+
+SET foreign_key_checks = 0;
+
+select * from constructionloanmanagement.address;
+
+select * from constructionloanmanagement.guideline;
